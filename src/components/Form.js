80366ad7b1322bff74f5b788439a679b1355b111
Alun Turner <alun.turner@googlemail.com>
2020-04-24 21:10:25 +0100

    complete game number entry form

---
+++ src/components/Form.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+class Form extends React.Component {
+  state = {
+    gameNumber: "",
+  };
+
+  updateLocalGameNumber = (event) => {
+    const input = event.target.value;
+    this.setState({ gameNumber: input });
+  };
+
+  render() {
+    return (
+      <form className="form-container">
+        <p className="form-header">ENTER GAME NUMBER:</p>
+        <input
+          className="form-input"
+          type="text"
+          onChange={this.updateLocalGameNumber}
+          value={this.state.gameNumber}
+        />
+        <button
+          className="form-button"
+          onClick={(e) => {
+            this.props.passGameNumber(e, this.state.gameNumber);
+          }}
+        >
+          FIND MY GAME
+        </button>
+      </form>
+    );
+  }
+}
+
+export default Form;
