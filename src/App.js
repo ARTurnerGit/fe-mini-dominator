c978aef090c278ef7f4b3e952f8286e1d6e03302
Alun Turner <alun.turner@googlemail.com>
2020-06-23 14:43:20 +0100

    setting up the logic to send the data requests

--- src/App.js
+++ src/App.js
@@ -69,6 +69,12 @@ class App extends React.Component {
       this.logWizard();
       this.countTerritoriesAndTroops();
     }
+    if (
+      prevState.gameConfirmed !== this.state.gameConfirmed &&
+      this.state.gameConfirmed
+    ) {
+      // send off all the requests and set the state
+    }
   }
 
   checkForFirstMentions = () => {
