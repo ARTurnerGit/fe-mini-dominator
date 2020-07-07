46fe26aa1e172378d77630644f76aa6b2350fe32
Alun Turner <alun.turner@googlemail.com>
2020-07-07 11:35:14 +0100

    form now in material-ui

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { TextField } from "@material-ui/core";
+import { Paper, TextField, Button } from "@material-ui/core";
 
 class Form extends React.Component {
   state = {
@@ -13,32 +13,25 @@ class Form extends React.Component {
 
   render() {
     return (
-      <form className="form-container">
+      <Paper className="form-container">
         <TextField
           label="Enter Game Number"
           variant="outlined"
-          onChange={this.updateLocalGameNumber}
           value={this.state.gameNumber}
-        />
-        <p className="form-header">ENTER GAME NUMBER:</p>
-        <input
-          className="form-input"
-          type="text"
           onChange={this.updateLocalGameNumber}
-          value={this.state.gameNumber}
         />
-        <button
-          className="form-button"
+        <Button
+          variant="contained"
           onClick={(e) => {
             this.props.passGameNumber(e, this.state.gameNumber);
           }}
         >
           FIND GAME
-        </button>
-        <button className="form-button" onClick={this.props.extractGameData}>
+        </Button>
+        <Button variant="contained" onClick={this.props.extractGameData}>
           CONFIRM
-        </button>
-      </form>
+        </Button>
+      </Paper>
     );
   }
 }
