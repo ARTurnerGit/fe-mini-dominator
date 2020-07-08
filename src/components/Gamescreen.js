d622e26ca3fb6b3ebb34d2f6a294e4c91afd0fd8
Alun Turner <alun.turner@googlemail.com>
2020-07-08 10:54:49 +0100

    sorted out the layout of the gamescreen

--- src/components/Gamescreen.js
+++ src/components/Gamescreen.js
@@ -19,8 +19,8 @@ function Gamescreen({ territories, map, players }) {
       style={{
         height: `${mapHeight * sf}px`,
         width: `${mapWidth * sf}px`,
-        zIndex: "-1",
         position: "relative",
+        display: "inline-block",
       }}
     >
       <img
