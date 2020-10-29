d7604f94b4f5bf55bece275c2dd449f05ac3a83d
Alun Turner <alun.turner@googlemail.com>
2020-10-29 08:12:58 +0000

    add GameContainer and connect to store

---
+++ src/components/containers/GameContainer.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { connect } from "react-redux";
+import Gamescreen from "../Gamescreen";
+import Sidebar from "../Sidebar";
+
+class GameContainer extends React.Component {
+  componentDidMount() {
+    // console.log(this.props.history[3]);
+  }
+
+  render() {
+    const {
+      map,
+      territories,
+      players,
+      roundCounter,
+      playerToGo,
+      msg,
+      playNextInLog,
+      handleReset,
+      logCounter,
+      logLength,
+    } = this.props;
+    return (
+      <div
+        className="game-container"
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          alignItems: "center",
+          width: "100vw",
+          height: "100vh",
+        }}
+      >
+        <Gamescreen map={map} territories={territories} players={players} />
+        <Sidebar
+          roundCounter={roundCounter}
+          playerToGo={playerToGo}
+          msg={msg}
+          playNextInLog={playNextInLog}
+          handleReset={handleReset}
+          logCounter={logCounter}
+          logLength={logLength}
+          players={players}
+        />
+      </div>
+    );
+  }
+}
+
+export default connect(
+  (state) => ({ history: state.board }),
+  null
+)(GameContainer);
--- src/pages/Game.js
+++ src/pages/Game.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Modal, CircularProgress } from "@material-ui/core";
 import Gamescreen from "../components/Gamescreen";
 import Sidebar from "../components/Sidebar";
+import GameContainer from "../components/containers/GameContainer";
 
 class Game extends React.Component {
   componentDidMount() {
@@ -34,28 +35,18 @@ class Game extends React.Component {
         <CircularProgress size="100px" style={{ outline: "none" }} />
       </Modal>
     ) : (
-      <div
-        className="game-container"
-        style={{
-          display: "flex",
-          justifyContent: "space-around",
-          alignItems: "center",
-          width: "100vw",
-          height: "100vh",
-        }}
-      >
-        <Gamescreen map={map} territories={territories} players={players} />
-        <Sidebar
-          roundCounter={roundCounter}
-          playerToGo={playerToGo}
-          msg={msg}
-          playNextInLog={playNextInLog}
-          handleReset={handleReset}
-          logCounter={logCounter}
-          logLength={logLength}
-          players={players}
-        />
-      </div>
+      <GameContainer
+        map={map}
+        territories={territories}
+        players={players}
+        roundCounter={roundCounter}
+        playerToGo={playerToGo}
+        msg={msg}
+        playNextInLog={playNextInLog}
+        handleReset={handleReset}
+        logCounter={logCounter}
+        logLength={logLength}
+      />
     );
   }
 }
