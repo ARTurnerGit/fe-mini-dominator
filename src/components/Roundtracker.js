80ad08cc587014b4cc50188d4b97305861e70e9c
Alun Turner <alun.turner@googlemail.com>
2020-10-31 15:37:13 +0000

    prep for Roundtracker reformat

--- src/components/Roundtracker.js
+++ src/components/Roundtracker.js
@@ -1,12 +1,40 @@
 import React from "react";
-import { Box, Typography } from "@material-ui/core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
+import { Box, Typography, Button } from "@material-ui/core";
 
 function Roundtracker({ roundCounter, playerToGo }) {
   return (
-    <Box style={{ height: "10vh" }}>
-      <Typography variant="body1" align="left">
-        ROUND: {roundCounter} <br /> {playerToGo}
-      </Typography>
+    <Box
+      className="roundTracker"
+      style={{
+        height: "10vh",
+        display: "flex",
+        justifyContent: "space-between",
+      }}
+    >
+      <div>
+        <Button variant="contained">
+          <FontAwesomeIcon icon={faMinus} />
+        </Button>
+        <Typography variant="body1" align="left">
+          ROUND: {roundCounter}
+        </Typography>
+        <Button variant="contained">
+          <FontAwesomeIcon icon={faPlus} />
+        </Button>
+      </div>
+      <div>
+        <Button variant="contained">
+          <FontAwesomeIcon icon={faMinus} />
+        </Button>
+        <Typography variant="body1" align="left">
+          {playerToGo}
+        </Typography>
+        <Button variant="contained">
+          <FontAwesomeIcon icon={faPlus} />
+        </Button>
+      </div>
     </Box>
   );
 }
